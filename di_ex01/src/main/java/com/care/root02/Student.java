cf628c8b8e996dbb78b8cb9d8c053124e6ebbd8d
HanGilyong <shkg13@naver.com>
2021-11-01 21:51:31 +0900

    1101_Spring_MVC2_설치_DI_xml

---
+++ di_ex01/src/main/java/com/care/root02/Student.java
@@ -0,0 +1,11 @@
+package com.care.root02;
+
+// 출력을 담당
+public class Student {
+	public void namePrint(String name) {
+		System.out.println("당신 이름 : "+name);
+	}
+	public void agePrint(int age) {
+		System.out.println("당신 이름 : "+age);
+	}
+}
